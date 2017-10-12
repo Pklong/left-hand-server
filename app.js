23ac5e675eb39451b8b7abaad0e5e1a3d45d3366
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-10-12 16:56:12 -0400

    wip

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require("express")
-const path = require("path")
+const express = require('express')
+const path = require('path')
 const app = express()
-const axios = require("axios")
-const config = require("./frontend/config")
+const axios = require('axios')
+const config = require('./frontend/config')
 
-app.use(express.static("frontend/dist"))
+app.use(express.static('frontend/dist'))
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "index.html"))
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 })
-app.get("/auth", (req, res) => {
+app.get('/auth', (req, res) => {
   axios
     .post(`https://github.com/login/oauth/access_token`, {
       client_id: config.Client_ID,
@@ -17,13 +17,14 @@ app.get("/auth", (req, res) => {
       code: req.query.code
     })
     .then(r => {
-      console.log(r)
-      res.sendFile(path.resolve(__dirname, "index.html"))
+      console.log('ACCESS TOKEN')
+      console.log(r.data)
+      res.end(r.data.split('=')[1].split('&')[0])
     })
     .catch(e => {
-      console.log(e)
+      console.log(e.message)
       res.end(e)
     })
 })
 
-app.listen(3000)
+app.listen(8080)
